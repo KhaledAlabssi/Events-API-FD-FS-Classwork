de41bcc6629dba8e4fe52dcfbe56ad8bd2b99e3e
KhaledAlabssi <khaled.absi5@gmail.com>
2025-07-05 15:41:40 +0200

    create auth form and style auth pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import AuthForm from '../components/AuthForm'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className='h-[550px]'>
+
+    <AuthForm />
+    </div>
   )
 }
