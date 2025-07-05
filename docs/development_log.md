1d9af5d0f0a7a0111270703f00c9a39b7ed057c5
KhaledAlabssi <khaled.absi5@gmail.com>
2025-07-05 16:08:15 +0200

    create and render dummy date in home page

--- docs/development_log.md
+++ docs/development_log.md
@@ -44,6 +44,13 @@
   5. stage, commit, and push the changes to github
   6. Switch to main branch
 
+    ## Branch: fetch
+  - Implementing task 3.1 form plan:
+    1. creating dummy data and render it from home page
+    2. setup home page with states and use effect
+    3. call the api and store the results in states
+    4. render the dynamic data on home page
+
 
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import EventCard from '../components/EventCard'
 
 export default function Home() {
+
+  const dd = {
+  "totalCount": 1,
+  "totalPages": 1,
+  "currentPage": 1,
+  "hasNextPage": false,
+  "hasPreviousPage": false,
+  "results": [
+    {
+      "id": 1,
+      "title": "Event Title",
+      "description": "Some Description for the Event",
+      "date": "2025-07-05T13:55:00.334Z",
+      "location": "Schloßbezirk 10, 76131 Karlsruhe",
+      "latitude": 8.404746955649602,
+      "longitude": 49.01438194665317,
+      "organizerId": 1,
+      "createdAt": "2025-07-05T13:55:00.334Z",
+      "updatedAt": "2025-07-05T13:55:00.334Z"
+    },
+    {
+      "id": 2,
+      "title": "Event2 Title",
+      "description": "Some2 Description for the Event",
+      "date": "2025-07-05T13:55:00.334Z",
+      "location": "Schloßbezirk 10, 76131 Karlsruhe",
+      "latitude": 8.404746955649602,
+      "longitude": 49.01438194665317,
+      "organizerId": 1,
+      "createdAt": "2025-07-05T13:55:00.334Z",
+      "updatedAt": "2025-07-05T13:55:00.334Z"
+    }
+  ]
+}
   return (
-    <div className=''>
-      <EventCard title={"hi"} date={"hi"} location={"hi"} descriptionSnippet={"hi"}/>
+    <div className='flex flex-col md:flex-row gap-4 flex-wrap md:justify-center p-8'>
+      {dd?.results?.map((i, k) =>  <EventCard title={i.title} date={i.date} location={i.location} descriptionSnippet={i.description}/>)}
+     
     </div>
   )
 }
